dc3c6b11694222ff943d99f20a7939dd9aa2fcad
ManuPedreros <manuelpedreros@msn.com>
2023-01-20 11:49:19 -0500

    Se Agregan características de ES8 como Object-entries y Object-values

---
+++ src/es8/01-object-values.js
@@ -0,0 +1,4 @@
+ // Devuelve un array con los valores correspondientes al objeto enumerable
+ const countries = {MX: 'México', CO: 'Colombia', CL: 'Chile', PE: 'Peru'};
+
+console.log(Object.values(countries));
